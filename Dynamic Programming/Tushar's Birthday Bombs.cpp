7e7c291054725b8134fd32988cfe1153d7706b7e
KARAN LAMBA <35367786+karan1808@users.noreply.github.com>
2019-07-04 12:47:18 +0530

    Add files via upload

---
+++ Dynamic Programming/Tushar's Birthday Bombs.cpp
@@ -0,0 +1,24 @@
+vector<int> Solution::solve(int A, vector<int> &B) {
+    vector<int> res;
+    int minpower=INT_MAX;
+    int minid=INT_MAX;
+    int n=B.size();
+    for(int i=0;i<n;i++){
+        if(minpower>B[i]){minid=i;minpower=B[i];}
+    }
+    int length=A/minpower;
+    for(int i=0;i<length;i++){
+        res.push_back(minid);
+    }
+    int rem=A-(length*minpower);
+    int j=0;
+    for(int i=0;i<length;i++){
+        for(int z=j;z<minid;z++){
+        if(B[z]-B[minid]<=rem&&j<minid){
+            res[i]=j;rem-=(B[j]-B[minid]);
+            break;
+        }else{j++;}
+        }
+    }
+ return res;   
+}
