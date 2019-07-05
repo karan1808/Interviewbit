14dffa8625e6fc809b38c61380eb50f19e97e753
KARAN LAMBA <35367786+karan1808@users.noreply.github.com>
2019-07-05 13:17:56 +0530

    Add files via upload

---
+++ Graph Data Structure & Algorithms/Level Order.cpp.cpp
@@ -0,0 +1,35 @@
+vector<vector<int> > Solution::levelOrder(TreeNode* A) {
+      vector<vector<int> > sol;
+    vector<int> temp;
+    queue<TreeNode*> q;
+    queue<int> level;
+    
+    if(A == NULL){
+        return sol;
+    }
+    
+    q.push(A);
+    level.push(0);
+    
+    while(!q.empty()){
+        TreeNode* curr = q.front();
+        int lev = level.front();
+        if(sol.size() == level.front()+1){
+        }else{
+            sol.push_back(temp);
+        }
+        sol[lev].push_back(curr->val);
+        if(curr->left != NULL){
+            q.push(curr->left);
+            level.push(lev+1);
+        }
+        if(curr->right != NULL){
+            q.push(curr->right);
+            level.push(lev+1);
+        }
+        q.pop();
+        level.pop();
+    }
+    
+    return sol;
+}
